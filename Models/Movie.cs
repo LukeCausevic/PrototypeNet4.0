95449973a325b5e8e8060f1ff9321158225adf6a
LukeCausevic <Lukecausevic@gmail.com>
2015-12-23 08:15:19 +1000

    Added entity framework and updated all nuget packages

---
+++ Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace PrototypeNet4._0.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class MovieDBContext : DbContext
+    {
+
+    }
+}
